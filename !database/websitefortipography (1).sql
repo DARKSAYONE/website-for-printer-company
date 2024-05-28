e8943bfc16dc4ece2f3d19384bcb01d978d8f25b
DARKSAYONE <skyhard2017@yandex.ru>
2024-05-29 00:21:15 +0300

    Create websitefortipography (1).sql

---
+++ !database/websitefortipography (1).sql
@@ -0,0 +1,181 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: localhost
+-- Время создания: Май 28 2024 г., 21:20
+-- Версия сервера: 8.0.36
+-- Версия PHP: 8.2.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `websitefortipography`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `phone` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `comment` text COLLATE utf8mb4_general_ci,
+  `service` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `status` enum('Принят','В обработке','Отменено','Выполнен') COLLATE utf8mb4_general_ci DEFAULT 'Принят',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `archived` tinyint(1) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Дамп данных таблицы `orders`
+--
+
+INSERT INTO `orders` (`id`, `name`, `phone`, `email`, `comment`, `service`, `status`, `created_at`, `archived`) VALUES
+(1, 'Сергей', '11111111', 'sergey095rare@gmail.com', 'Здравствуйте, хочу.', 'Визитка', 'Выполнен', '2024-05-28 20:26:34', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int NOT NULL,
+  `text` text COLLATE utf8mb4_general_ci NOT NULL,
+  `image` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Дамп данных таблицы `posts`
+--
+
+INSERT INTO `posts` (`id`, `text`, `image`, `created_at`) VALUES
+(15, 'Книга \"Удивительный мир насекомых\"', 'XXXL (5).webp', '2024-05-28 14:51:11'),
+(16, 'Каталог для \"Tesla Premium Oil\"', 'XXXL (4).webp', '2024-05-28 14:51:40'),
+(17, 'Визитка \"ED Project\"', 'XXXL (3).webp', '2024-05-28 14:52:12'),
+(18, 'Заказной календарь \"Лето 2021\"', 'XXXL (2).webp', '2024-05-28 14:52:38'),
+(19, 'Сертификат студии правильного маникюра.', 'XXXL.webp', '2024-05-28 14:53:10'),
+(20, 'Канцелярия для \"КМВ Новотерская\"', 'XXXL (1).webp', '2024-05-28 14:53:45'),
+(21, 'Визитка для \"Yakov\"', 'XXXL (7).webp', '2024-05-28 14:55:14'),
+(22, 'Приглашение на мероприятие \"Flamingo\"', 'XXXL (6).webp', '2024-05-28 14:57:24'),
+(23, 'Меню для кафе-бара \"Малина\"', 'XXXL (8).webp', '2024-05-28 14:58:04');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `services`
+--
+
+CREATE TABLE `services` (
+  `id` int NOT NULL,
+  `title` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `description` text COLLATE utf8mb4_general_ci NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `image` varchar(255) COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Дамп данных таблицы `services`
+--
+
+INSERT INTO `services` (`id`, `title`, `description`, `price`, `created_at`, `image`) VALUES
+(1, 'Визитка', 'Визиточка', 1000.00, '2024-05-28 15:17:19', '1716909439_1.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user`
+--
+
+CREATE TABLE `user` (
+  `id` int NOT NULL,
+  `admin` tinyint NOT NULL,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `pass` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Дамп данных таблицы `user`
+--
+
+INSERT INTO `user` (`id`, `admin`, `username`, `email`, `pass`, `created`) VALUES
+(42, 1, 'Оператор', 'sergey095rare@gmail.com', '$2y$10$t1h/eRiLVfc41qw/e9T.3.HPPcQZD1S46Iu8mwdxjvq0h9ISpeXhy', '2024-05-28 20:59:00');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `services`
+--
+ALTER TABLE `services`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT для таблицы `services`
+--
+ALTER TABLE `services`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
